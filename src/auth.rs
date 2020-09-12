a3422a13e1222667d851df71aea76c6f12a0554d
Rob Ede <robjtede@icloud.com>
2020-09-12 04:09:05 +0100

    update actix-web dependency to 3.0.0

--- src/auth.rs
+++ src/auth.rs
@@ -58,7 +58,7 @@ pub fn get_identity_service() -> IdentityService<CookieIdentityPolicy> {
     IdentityService::new(
         CookieIdentityPolicy::new(&CONFIG.session_key.as_ref())
             .name(&CONFIG.session_name)
-            .max_age_time(chrono::Duration::minutes(CONFIG.session_timeout))
+            .max_age_time(time::Duration::minutes(CONFIG.session_timeout))
             .secure(CONFIG.session_secure),
     )
 }
